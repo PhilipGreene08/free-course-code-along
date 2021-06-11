bbd8926dd73dd73ac257f8be053f31134bd37777
Philip Grene <54162361+PhilipGreene08@users.noreply.github.com>
2021-06-10 21:34:12 -0500

    New Code
    Working to do this project on my own instead of coding along.

--- free-course-code-along/js/app.js
+++ free-course-code-along/js/app.js
@@ -2,30 +2,31 @@
 
 (function () {
     document.addEventListener('DOMContentLoaded', e => {
+        e.preventDefault()
         const display = new Display()
         display.checkFields()
         display.hideSubmit()
     })
 
-
-    document.getElementById('customer-form').addEventListener('submit', function (e) {
+    document.getElementById('customer-form').addEventListener('submit', e => {
         e.preventDefault()
 
-        const name = this.querySelector('.name');
-        const course = this.querySelector('.course');
-        const author = this.querySelector('.author');
+        const name = document.querySelector('.name');
+        const course = document.querySelector('.course');
+        const author = document.querySelector('.author');
 
-        const customer = new Customer(name.value, course.value, author.value)
+
         const display = new Display()
+        const customer = new Customer(name.value, course.value, author.value)
 
         display.feedback(customer)
-        display.clearFields()
+        console.log(customer);
     })
 
     function Display() {
-        this.name = document.querySelector('.name')
-        this.course = document.querySelector('.course')
-        this.author = document.querySelector('.author')
+        this.name = document.querySelector('.name');
+        this.course = document.querySelector('.course');
+        this.author = document.querySelector('.author');
         this.customers = document.querySelector('.customer-list')
     }
 
@@ -35,56 +36,31 @@
         this.author.addEventListener('blur', this.validateField)
     }
 
-    Display.prototype.hideSubmit = function () {
-        const btn = document.querySelector('.submitBtn')
-        btn.disabled = true
-    }
-
     Display.prototype.validateField = function () {
         if (this.value === ``) {
-            this.classList.remove('complete');
-            this.classList.add('fail');
+            this.classList.remove('complete')
+            this.classList.add('fail')
         } else {
-            this.classList.add('complete');
-            this.classList.remove('fail');
+            this.classList.add('complete')
+            this.classList.remove('fail')
         }
 
         const complete = document.querySelectorAll('.complete')
 
         if (complete.length === 3) {
-            document.querySelector('.submitBtn').disabled = false;
+            document.querySelector('.submitBtn').disabled = false
         } else {
-            document.querySelector('.submitBtn').disabled = true;
+            document.querySelector('.submitBtn').disabled = true
         }
     }
 
     Display.prototype.feedback = function (customer) {
-        const feedback = document.querySelector('.feedback')
-        const loading = document.querySelector('.loading')
 
-        feedback.classList.add('showItem', 'alert', 'alert-success')
-        loading.classList.add('showItem')
-
-        const self = this
-        //console.log(self);
-        self.hideSubmit()
-
-        setTimeout(function () {
-            feedback.classList.remove('showItem', 'alert', 'alert-success')
-            loading.classList.remove('showItem')
-            self.addCustomer(customer)
-        }, 3000)
-        //console.log(this);
     }
 
-    Display.prototype.clearFields = function () {
-        this.name.value = ``
-        this.course.value = ``
-        this.author.value = ``
-
-        this.name.classList.remove('complete', 'fail');
-        this.course.classList.remove('complete', 'fail');
-        this.author.classList.remove('complete', 'fail');
+    Display.prototype.hideSubmit = function () {
+        const btn = document.querySelector('.submitBtn')
+        btn.disabled = true
     }
 
     function Customer(name, course, author) {
@@ -96,51 +72,31 @@
     Display.prototype.addCustomer = function (customer) {
         const random = this.getRandom()
         const div = document.createElement('div')
-        div.classList.add('col-11', 'mx-auto', 'col-md-6', 'my-3', 'col-lg-4');
-        div.innerHTML = `<div class="card text-left">
-     <img src="./img/cust-${random}.jpg" class="card-img-top" alt="">
-     <div class="card-body">
-      <!-- customer name -->
-      <h6 class="text-capitalize "><span class="badge badge-warning mr-2">name :</span><span id="customer-name">${customer.name}</span></h6>
-      <!-- end of customer name -->
-      <!-- customer name -->
-      <h6 class="text-capitalize my-3"><span class="badge badge-success mr-2">course :</span><span id="customer-course">
-        ${customer.course}
-       </span></h6>
-      <!-- end of customer name -->
-       <!-- customer name -->
-       <h6 class="text-capitalize"><span class="badge badge-danger mr-2">author :</span><span id="course-author">${customer.author}</span></h6>
-       <!-- end of customer name -->
+        div.classList.add('col-11', 'mx-auto', 'col-md-6', 'my-3', 'col-lg-4')
+        div.innerHTML = `
+        <div class="card text-left">
+            <img src="./img/cust-${random}.jpg" class="card-img-top" alt="">
+                <div class="card-body">
+        <!-- customer name -->
+                    <h6 class="text-capitalize "><span class="badge badge-warning mr-2">name :</span><span id="customer-name">${customer.name}</span></h6>
+        <!-- end of customer name -->
+        <!-- customer name -->
+                    <h6 class="text-capitalize my-3"><span class="badge badge-success mr-2">course :</span><span id="customer-course">
+          ${customer.course}
+                    </span></h6>
+        <!-- end of customer name -->
+        <!-- customer name -->
+                <h6 class="text-capitalize"><span class="badge badge-danger mr-2">author :</span><span id="course-author">${customer.author}</span></h6>
+        <!-- end of customer name -->
+                </div>
       </div>
-    </div>`
-
+        `
+        //const customerList = document.querySelector('.customer-list')
         this.customers.appendChild(div)
     }
 
     Display.prototype.getRandom = function () {
-        let random = Math.floor(Math.random() * 5 + 1)
+        let random = Math.random() * (max - min) + min
         return random
     }
-
-
-
 })();
-
-// Display.prototype.clearFields = function(){
-//     this.name.value = '';
-//     this.course.value = '';
-//     this.author.value = '';
-
-//     this.name.classList.remove('complete', 'fail');
-//     this.course.classList.remove('complete', 'fail');
-//     this.author.classList.remove('complete', 'fail');
-// };
-
-// //customer constructor function
-// function Customer(name, course, author){
-//     this.name = name;
-//     this.course = course;
-//     this.author = author;
-// };
-
-// })()
